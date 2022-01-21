70d27d9a7111136756da5f116e637727193219d0
Andrei Slusari <andrei.slusari.2020@gmail.com>
2022-01-21 04:47:34 +0300

    completed nftc-frontend

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import contract from './contracts/NFTCollectible.json';
+import { ethers } from 'ethers';
+
+const contractAddress = "0xC43b2f71399Fa5299e5fb511276831d1Af437cc0";
+const abi = contract.abi;
+
+function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = () => { }
+
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        let nftTxn = await nftContract.mintNFTs(1, { value: ethers.utils.parseEther("0.01") });
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint NFT
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
+  return (
+    <div className='main-app'>
+      <h1>Scrappy Squirrels Tutorial</h1>
+      <div>
+        {currentAccount ? mintNftButton() : connectWalletButton()}
+      </div>
+    </div>
+  )
+}
+
+export default App;
